3d008083fb91a30f93dee4d57aad932d347abb9a
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2024-05-08 15:03:52 -0300

    Add files via upload

---
+++ ProgAulaIII_Heraranca/src/progaulaiii_hreranca/ProgAulaIII_Hreranca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progaulaiii_hreranca;
+
+/**
+ *
+ * @author Márcio
+ */
+public class ProgAulaIII_Hreranca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ProgAulaIII_Heraranca/src/progaulaiii_hreranca/entidades/Pessoa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progaulaiii_hreranca.entidades;
+
+/**
+ *
+ * @author Márcio
+ */
+public class Pessoa {
+    private Long id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+}
+
